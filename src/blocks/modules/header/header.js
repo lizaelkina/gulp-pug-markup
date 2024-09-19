b4d931c12911d13d062af29db283bb8c28c89c77
Liza Elkina <lizapetkova@yandex.ru>
2024-09-19 17:59:15 +0300

    fix: urls for gh-pages

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,6 +1,6 @@
 const menuList = document.querySelectorAll('.menu__item');
 menuList.forEach(menuItem => {
-  const url = menuItem.getAttribute('href');
+  const url = menuItem.getAttribute('href').substring(1);
   if (window.location.href.endsWith(url)) {
     menuItem.classList.add('menu__item_active');
   }
